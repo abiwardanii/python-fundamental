ab1bbc160174bc0a1ba9773869d32f6b840b8d17
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-11-03 12:47:09 +0700

    update input

--- input.py
+++ input.py
@@ -1,3 +1,7 @@
 name = input("siapa nama kamu? ")
 print("nama saya adalah " + name)
 print(type(name))
+
+age = int(input("berapa umur kamu? ")) 
+print("umur saya adalah " + str(age))
+print(type(age))
