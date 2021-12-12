74fd6f5f045ceb4becd075594f0155e74212ab7d
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-12-12 20:16:12 +0700

    advanced number

---
+++ advanced-number/index.py
@@ -0,0 +1,17 @@
+#hex
+print(hex(256))
+
+#bin
+print(bin(256))
+
+#pow
+print(pow(2,3))
+print(pow(2,4,3))
+
+#abs
+print(abs(-10))
+print(abs(10))
+
+#round
+print(round(3.1415926,2))
+print(round(3.1415926))
